3852f7df2018a871f7eb298b95af57020caa9c6e
Hanzala Siddiqui <prodevelop110@gmail.com>
2025-11-29 11:26:00 +0500

    Update app.js

--- Filter Table/app.js
+++ Filter Table/app.js
@@ -1,22 +1,18 @@
 let searchInput = document.getElementById("search");
 let roleSelect = document.getElementById("roleFilter");
 let rows = document.querySelectorAll("#dataTable tbody tr");
-
 function filterTable() {
   let text = searchInput.value.toLowerCase();
   let role = roleSelect.value.toLowerCase();
-
   rows.forEach(row => {
     let name = row.cells[0].textContent.toLowerCase();
     let job = row.cells[1].textContent.toLowerCase();
     let city = row.cells[2].textContent.toLowerCase();
-
     let matchText = name.includes(text) || job.includes(text) || city.includes(text);
     let matchRole = !role || job.includes(role);
-
     row.style.display = (matchText && matchRole) ? "" : "none";
   });
 }
-
 searchInput.addEventListener("input", filterTable);
 roleSelect.addEventListener("change", filterTable);
+
